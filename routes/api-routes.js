27354754757630417252d463520a84d202fa026f
treighton <treighton.mauldin@get10up.com>
2021-01-11 18:53:45 -0800

    lint demo

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,6 +3,7 @@ const db = require("../models");
 const passport = require("../config/passport");
 
 module.exports = function(app) {
+  const un_used_var = 'im a string';
   // Using the passport.authenticate middleware with our local strategy.
   // If the user has valid login credentials, send them to the members page.
   // Otherwise the user will be sent an error
